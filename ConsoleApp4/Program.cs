ae0c385426fd3b08006c3edb9ca89b61cbda046e
KenanNagiyev <788lnmp@code.edu.az>
2022-03-05 08:18:22 +0400

    new projekt

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ededler = { 8, 4, -2, 3,-6 , 5, 9 };
+            int min = ededler[0];
+            for (int i = 0; i < ededler.Length; i++)
+            {
+                if (ededler[i]<min)
+                {
+                    min = ededler[i];
+                }
+            }
+            Console.WriteLine(min);
+
+        }
+    }
+}
